7dcf581332ce0d678252854b3904e0a607030d5f
aimartians2 <aimartians2@gmail.com>
2023-08-29 10:52:52 +0530

    commit

---
+++ abc/calculator.py
@@ -0,0 +1,29 @@
+# import mohan
+# import sohan
+# mohan.add(10,20)
+# mohan.div(10,20)
+# mohan.sub(10,20)
+# sohan.multi(10,20)
+
+
+# import mohan as m
+# import sohan as s
+# m.add(10,20)
+# m.div(10,20)
+# m.sub(10,20)
+# s.multi(10,20)
+
+
+# from mohan import *
+# from sohan import *
+# add(10,20)
+# div(10,20)
+# sub(10,20)
+# multi(10,20)
+
+from xyz.mohan import add,sub,div
+from xyz.sohan import multi
+add(10,20)
+div(10,20)
+sub(10,20)
+multi(10,20)
---
+++ abc/xyz/sohan.py
@@ -0,0 +1,9 @@
+def sub(a,b):
+    print(a-b)
+
+def div(a,b):
+    print(a/b)
+
+def multi(a,b):
+    print(a*b)
+    
